d254583314564a961049508963ceacc62934ed04
saengle <zohv817@gmail.com>
2024-03-12 21:19:36 +0900

    Add: 기본 틀

--- BaseBallGame/main.swift
+++ BaseBallGame/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+//class Input { // 인풋 컨트롤
+//    var input01: [Int] = []
+//    
+//    func read() {
+//        input01 = readLine()!.split(separator: " ").map { Int(String($0))!}
+//        let input1 = readLine()!.split(separator: " ").map { String($0)}
+//        let input2 = readLine()!.split(separator: " ").map { Int(String($0))!}
+//    }
+//}
 
+class BaseBall { // 게임 컨트롤
+//    let input = Input()
+    func start() {
+//        input.read()
+//        print(input.input01)
+        
+    }
+}
+
+let baseball = BaseBall()
+baseball.start()
