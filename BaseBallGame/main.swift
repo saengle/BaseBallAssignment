d7842d3f4c1a0f1caf1bb2d079f1379e8ba431ea
saengle <zohv817@gmail.com>
2024-03-13 23:06:45 +0900

    Add: Lv2 숫자 받아서 게임 진행

--- BaseBallGame/main.swift
+++ BaseBallGame/main.swift
@@ -7,24 +7,38 @@
 
 import Foundation
 
-//class Input { // 인풋 컨트롤
-//    var input01: [Int] = []
-//    
-//    func read() {
-//        input01 = readLine()!.split(separator: " ").map { Int(String($0))!}
-//        let input1 = readLine()!.split(separator: " ").map { String($0)}
-//        let input2 = readLine()!.split(separator: " ").map { Int(String($0))!}
-//    }
-//}
+class Input { // 인풋 컨트롤
+    var input01: Array<Int> = []
+    
+    func read() {
+        input01 = readLine()!.split(separator: "").map { Int(String($0))!}
+
+        
+    }
+    
+    func checkGameNum() {
+//        if input01 == ["q"] {
+//            //quit
+//        }
+        
+//        if input01
+    }
+    
+    func checkMainMenu() {
+        
+    }
+}
 
 class BaseBall { // 게임 컨트롤
     
-//    let input = Input()
+    let input = Input()
     func start() {
 //        input.read()
 //        print(input.input01)
         let answer = makeAnswer()
-        print(answer)
+        
+        
+        checkAnswer(answer)
     }
     
     func makeAnswer() -> Array<Int> {
@@ -46,12 +60,44 @@ class BaseBall { // 게임 컨트롤
 
         func makeThirdNum() {
             while numSet.count == 2 {
-                thirdNum =  Int.random(in: 0...9)
+                thirdNum = Int.random(in: 0...9)
                 numSet.insert(thirdNum)
             }
         }
         numArray = [firstNum, secondNum, thirdNum]
         return numArray
+    }
+    
+    func checkAnswer(_ answer: Array<Int>) {
+        input.read()
+        let myArr: Array<Int> = input.input01
+        print(myArr, answer)
+        var ballCount: Int = 0
+        var strikeCount: Int = 0
+        var cnt: Int = 0
+        var temp: Int = 0
+        for i in 0...2 {
+            if answer.contains(myArr[i]) {
+                ballCount += 1
+            }
+            if answer[i] == myArr[i] {
+                ballCount -= 1
+                strikeCount += 1
+            }
+        }
+        
+        
+        
+        
+        print(strikeCount, ballCount)
+        if myArr == [0, 0, 0] {
+            print("게임을 종료합니다.")
+            return
+        } else if strikeCount <= 2 {
+            checkAnswer(answer)
+        } else if strikeCount == 3 {
+            print("축하합니다 정답입니다.")
+        }
     }
 }
 
