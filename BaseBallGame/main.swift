a1d08dc6370311b6c08e7e91a7e8dfc783e29a9f
saengle <zohv817@gmail.com>
2024-03-14 17:23:53 +0900

    Fix: Error 게임스코어 관련 에러 수정

--- BaseBallGame/main.swift
+++ BaseBallGame/main.swift
@@ -7,10 +7,7 @@
 
 import Foundation
 
-class Input { /* 인풋 컨트롤.  일단 스트링으로 받아와서 다 떨어트려서 세트화
-               - 카운트로 개수 3개 아니면 잘못된거다 프린트, 다시실행.
-               개수 3개 맞으면 세트로 0 - 9, 1 - 3 까지만 있는 배열에 넣고 숫자가 달라지면 잘못되었다.
-               0000 은 특이케이스로 게임중에 먼저 입력받으면 게임 종료(메뉴로 복귀).
+class Input { /* 인풋 컨트롤.
                ***** 밑에 이프 렛 이용해서 입력값을 받아오는건 성공인데, "quit" 이면 나가기(메인메뉴) 하고싶은데 어떻게 받고 확인하지 ?
                */
     var input01: Array<Int> = []
@@ -97,7 +94,7 @@ class BaseBall { // 게임 컨트롤
         
         var ballCount: Int = 0
         var strikeCount: Int = 0
-        print(answer)   // 임시
+//        print(answer)   // 임시로 사용 할때만 주석 제거
         for i in 0...2 {
             if answer.contains(myArr[i]) {
                 ballCount += 1
@@ -117,12 +114,12 @@ class BaseBall { // 게임 컨트롤
                 """)
             checkAnswer(answer)
         } else if strikeCount == 3 {
+            cnt += 1
             print("""
                 \(cnt) 번째 시도
                 축하합니다 정답입니다.
                 메인 화면으로 돌아갑니다.
                 """)
-            cnt += 1
             cntArray.append(cnt)
             cnt = 0
             mainMenu()
@@ -151,8 +148,11 @@ class BaseBall { // 게임 컨트롤
     
     func gameScore() {
         print("게임 스코어입니다.")
-        for i in 1...cntArray.count {
-            print("\(i)번째 \(cntArray[i])회")
+        if cntArray.count == 1 {print("1번째 게임 \(cntArray[0])회차 성공")
+        } else  {
+            for i in 1...cntArray.count {
+                print("\(i)번째 게임 \(cntArray[i-1])회차 성공")
+            }
         }
         print("메인 메뉴로 돌아갑니다.")
         mainMenu()
